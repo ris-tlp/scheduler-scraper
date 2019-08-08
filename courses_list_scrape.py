4c287dc4947cb869f8e6d63c7b89ad8ea8afbfd7
Omar <omarkhantlp@gmail.com>
2019-08-08 03:41:39 +0300

    returns courses for 3 terms

--- courses_list_scrape.py
+++ courses_list_scrape.py
@@ -6,6 +6,8 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 def returnCourses():
+    '''Returns a list of all available courses'''
+
     options = soup.find(id = "CntntPlcHldr_ddlDept")
     courses = []
 
@@ -15,11 +17,24 @@ def returnCourses():
     return(courses)
 
 def returnFormAttributes():
+    '''Returns form attributes necessary for submission'''
+
     __VIEWSTATEGENERATOR = soup.find("input", id = "__VIEWSTATEGENERATOR").get("value")
     __VIEWSTATE = soup.find("input", id = "__VIEWSTATE").get("value")
     __EVENTVALIDATION = soup.find("input", id = "__EVENTVALIDATION").get("value")
 
     return __VIEWSTATEGENERATOR, __VIEWSTATE, __EVENTVALIDATION
+
+def returnLatestTerms():
+    '''Returns a list of the 3 most recent terms'''
+    
+    terms = []
+    
+    for term in soup.find(id = "CntntPlcHldr_ddlTerm").find_all("option"):
+        terms.append(term.get("value"))
+
+    return terms[:3]
+
 
 
 if __name__ == "__main__":
--- post_form.py
+++ post_form.py
@@ -1,37 +1,40 @@
-from courses_list_scrape import returnCourses, returnFormAttributes
+from courses_list_scrape import returnCourses, returnFormAttributes, returnLatestTerms
 from bs4 import BeautifulSoup
 import requests
 
 courses = returnCourses()
+terms = returnLatestTerms()
 __VIEWSTATEGENERATOR, __VIEWSTATE, __EVENTVALIDATION = returnFormAttributes() 
 courseData = {}
 
-for course in courses:
-    payload = {
-        '__VIEWSTATE': __VIEWSTATE,
-        '__VIEWSTATEGENERATOR': __VIEWSTATEGENERATOR,
-        '__EVENTVALIDATION': __EVENTVALIDATION,
-        'ctl00$CntntPlcHldr$ddlTerm': '201910',
-        'ctl00$CntntPlcHldr$ddlDept': course
-        }
-    url = "https://registrar.kfupm.edu.sa/CourseOffering"
+for term in terms:
+    for course in courses:
+        payload = {
+            '__VIEWSTATE': __VIEWSTATE,
+            '__VIEWSTATEGENERATOR': __VIEWSTATEGENERATOR,
+            '__EVENTVALIDATION': __EVENTVALIDATION,
+            'ctl00$CntntPlcHldr$ddlTerm': term,
+            'ctl00$CntntPlcHldr$ddlDept': course
+            }
+        url = "https://registrar.kfupm.edu.sa/CourseOffering"
 
-    response = requests.post(url, data=payload)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    data = []
+        response = requests.post(url, data=payload)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        data = []
 
-    for row in soup.find_all("div", class_="trow"):
-        data.append([]) #creating list of lists of each course
+        for row in soup.find_all("div", class_="trow"):
+            data.append([]) #creating list of lists of each course
 
-        for inner in row.find_all("div", class_="tdata"):
-            index = inner.text.index(":")
-            tagContent = inner.text[index+1:] #slicing to get only content
-            data[len(data) - 1].append(tagContent) #appending to end of list
-    
-    courseData[course] = data #setting data according to course abbrev
+            for inner in row.find_all("div", class_="tdata"):
+                index = inner.text.index(":")
+                tagContent = inner.text[index+1:] #slicing to get only content
+                data[len(data) - 1].append(tagContent) #appending to end of list
+        
+        courseData[course] = data #setting data according to course abbrev
 
 # output test
-# for key, value in courseData.items():
-#     print(f'----------  {key}  ---------- ')
-#     for _ in value:
-#         print(_)
+    # for key, value in courseData.items():
+    #     print(f' ----------  {key}  ---------- ')
+    #     for _ in value:
+    #         print(_)
+    # print(f'------------------- {term} -------------------')
