acd4592d8edace56bbfff78a9886bbf8d3afd68d
Ajmal Ali <ajmalsyedali@gmail.com>
2019-08-08 16:37:32 +0530

    Optimize data extraction

--- post_form.py
+++ post_form.py
@@ -4,18 +4,18 @@ import requests
 
 courses = returnCourses()
 terms = returnLatestTerms()
-__VIEWSTATEGENERATOR, __VIEWSTATE, __EVENTVALIDATION = returnFormAttributes() 
+__VIEWSTATEGENERATOR, __VIEWSTATE, __EVENTVALIDATION = returnFormAttributes()
 courseData = {}
 
-for term in terms:
-    for course in courses:
+for term in terms[:1]:
+    for course in courses[:2]:
         payload = {
             '__VIEWSTATE': __VIEWSTATE,
             '__VIEWSTATEGENERATOR': __VIEWSTATEGENERATOR,
             '__EVENTVALIDATION': __EVENTVALIDATION,
             'ctl00$CntntPlcHldr$ddlTerm': term,
             'ctl00$CntntPlcHldr$ddlDept': course
-            }
+        }
         url = "https://registrar.kfupm.edu.sa/CourseOffering"
 
         response = requests.post(url, data=payload)
@@ -23,18 +23,18 @@ for term in terms:
         data = []
 
         for row in soup.find_all("div", class_="trow"):
-            data.append([]) #creating list of lists of each course
+            data.append([])  # creating list of lists of each course
 
             for inner in row.find_all("div", class_="tdata"):
-                index = inner.text.index(":")
-                tagContent = inner.text[index+1:] #slicing to get only content
-                data[len(data) - 1].append(tagContent) #appending to end of list
-        
-        courseData[course] = data #setting data according to course abbrev
+                tagContent = inner.span.next_sibling
+                # appending to end of list
+                data[len(data) - 1].append(tagContent)
+
+        courseData[course] = data  # setting data according to course abbrev
 
 # output test
-    # for key, value in courseData.items():
-    #     print(f' ----------  {key}  ---------- ')
-    #     for _ in value:
-    #         print(_)
-    # print(f'------------------- {term} -------------------')
+# for key, value in courseData.items():
+#     print(f' ----------  {key}  ---------- ')
+#     for _ in value:
+#         print(_)
+# print(f'------------------- {term} -------------------')
