641d748dad8c30ffd6bf6ea2885d798a2de00391
Omar <omarkhantlp@gmail.com>
2019-10-18 15:57:27 +0300

    updated unittests

--- tests.py
+++ tests.py
@@ -1,51 +1,40 @@
 import unittest
 import requests
 from bs4 import BeautifulSoup
-from courses_list_scrape import getDepartments, getFormAttributes, getTerms
+from scraper import Scraper
 
-url = "https://registrar.kfupm.edu.sa/CourseOffering"
-__VIEWSTATEGENERATOR, __VIEWSTATE, __EVENTVALIDATION = getFormAttributes()
-payload = {
-            '__VIEWSTATE': __VIEWSTATE,
-            '__VIEWSTATEGENERATOR': __VIEWSTATEGENERATOR,
-            '__EVENTVALIDATION': __EVENTVALIDATION,
-            'ctl00$CntntPlcHldr$ddlTerm': getTerms(limit=1),
-            'ctl00$CntntPlcHldr$ddlDept': "ICS"
-        }
-response = requests.post(url, data=payload)
+scraper = Scraper()
+scraper.setTerms(2)
+scraper.setDepartments()
+scraper.setFormAttributes()
+
+payload = scraper.setPayload(scraper.terms, scraper.depts)
+response = requests.post(scraper.url, data=payload)
 soup = BeautifulSoup(response.text, 'html.parser')
+
 
 class Tests(unittest.TestCase):
 
-    def testResponseIsNotNone(self):
+    def test_response_is_not_none(self):
         self.assertIsNotNone(response)
     
-    def testAttributesIsNotNone(self):
-        __VIEWSTATEGENERATOR, __VIEWSTATE, __EVENTVALIDATION = getFormAttributes()
-        self.assertIsNotNone(__VIEWSTATE)
-        self.assertIsNotNone(__VIEWSTATEGENERATOR)
-        self.assertIsNotNone(__EVENTVALIDATION)
+    def test_depts_is_not_none(self):
+        self.assertIsNotNone(scraper.depts)
 
-    def testDeptsIsNotNone(self):
-        depts = getDepartments()
-        self.assertIsNotNone(depts)
+    def test_terms_is_not_none(self):
+        self.assertIsNotNone(scraper.terms)
 
-    def testTermsIsNotNone(self):
-        terms = getTerms()
-        self.assertIsNotNone(terms)
-
-    def testFormStructure(self):
+    def test_form_structure(self):
         self.assertIsNotNone(soup.find(id="CntntPlcHldr_ddlDept"))
         self.assertIsNotNone(soup.find(id="CntntPlcHldr_ddlTerm"))
         #list will be false if empty
         self.assertTrue(soup.find(id="CntntPlcHldr_ddlTerm").find_all("option"))
         self.assertTrue(soup.find(id="CntntPlcHldr_ddlDept").find_all("option"))
     
-    def testDataTableStructure(self):
+    def test_data_table_structure(self):
         #list will be false if empty
         self.assertTrue(soup.find_all("div", class_="trow"))
         self.assertTrue(soup.find_all("div", class_="tdata"))
-        
 
 
 if __name__ == "__main__":
