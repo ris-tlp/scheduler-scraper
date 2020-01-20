f9bcb978c1f748ccd4e666c3d8f340dd52bae87a
Omar <omarkhantlp@gmail.com>
2020-01-20 04:05:54 +0300

    added courses resource and course/term/all path

--- courseoffering/app.py
+++ courseoffering/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from courseoffering.resources.HelloWorld import HelloWorld
 from courseoffering.resources.terms import Terms
+from courseoffering.resources.courses import Courses
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(HelloWorld, "/")
 api.add_resource(Terms, "/terms/", "/terms/<int:limit>")
+api.add_resource(Courses, "/courses/<string:term>/all")
+
--- courseoffering/resources/courses.py
+++ courseoffering/resources/courses.py
@@ -1,4 +1,19 @@
 from flask_restful import Resource
 from courseoffering.utils.database import session
+from courseoffering.models.courses import Course
 from flask import jsonify
 
+
+class Courses(Resource):
+
+    def get(self, term):
+
+        # Fetches all terms with corrected Term format (192 -> 201920)
+        courses = [course.code for course in session.query(Course).filter(
+            Course.term == (f"20{term}0" if len(term) != 6 else term)
+        )]
+
+        if not courses:
+            return jsonify({"Courses": "None"})
+        else:
+            return jsonify({"Courses": courses})
